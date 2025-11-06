0ad586d2e3a82f634956bb9e9471860352796e6a
IC\ms1923 <mahdi97779@gmail.com>
2025-11-06 12:54:04 +0000

    Initialisation code

--- main.s
+++ main.s
@@ -2,23 +2,19 @@
 
 psect	code, abs
 	
-main:
-	org	0x0
-	goto	start
-
-	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movf PORTD, W, A
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
-
-	end	main
+SPI_MasterInit: ; Set Clock edge to negative
+	bcf CKE2 ; CKE bit in SSP2STAT,
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw (SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+	movwf SSP2CON1, A
+	; SDO2 output; SCK2 output
+	bcf TRISD, PORTD_SDO2_POSN, A ; SDO2 output
+	bcf TRISD, PORTD_SCK2_POSN, A ; SCK2 output
+	return
+SPI_MasterTransmit: ; Start transmission of data (held in W)
+	movwf SSP2BUF, A ; write data to output buffer
+Wait_Transmit: ; Wait for transmission to complete
+	btfss PIR2, 5 ; check interrupt flag to see if data has been sent
+	bra Wait_Transmit
+	bcf PIR2, 5 ; clear interrupt flag
+	return
