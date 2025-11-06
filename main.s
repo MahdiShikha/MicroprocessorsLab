06215e4524832b65fd05affbf6fc6f42ccf400d7
IC\ms1923 <mahdi97779@gmail.com>
2025-11-06 15:55:48 +0000

    2nd attempt - two led values

--- main.s
+++ main.s
@@ -1,44 +1,83 @@
-	#include <xc.inc>
+; =========================
+; Minimal SPI2 demo (PIC18)
+; RD4 = SDO2  -> 74HC164 A (data)
+; RD6 = SCK2  -> 74HC164 CLK (rising-edge sample)
+; =========================
+#include <xc.inc>
 
-psect	code, abs
-	goto Start
-SPI_MasterInit: ; Set Clock edge to negative
-	bcf CKE2 ; CKE bit in SSP2STAT,
-	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
-	movlw (SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
-	movwf SSP2CON1, A
-	; SDO2 output; SCK2 output
-	bcf TRISD, PORTD_SDO2_POSN, A ; SDO2 output
-	bcf TRISD, PORTD_SCK2_POSN, A ; SCK2 output
-	return
-SPI_MasterTransmit: ; Start transmission of data (held in W)
-	movwf SSP2BUF, A ; write data to output buffer
-Wait_Transmit: ; Wait for transmission to complete
-	btfss PIR2, 5 ; check interrupt flag to see if data has been sent
-	bra Wait_Transmit
-	bcf PIR2, 5 ; clear interrupt flag
-	return
-Table:
-	db 0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80
-	myArray EQU 0x400	; Address in RAM for data
-	counter EQU 0x10	; Address of counter variable
-	align	2		; ensure alignment of subsequent instructions
-Start:
-	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(Table)	; address of data in PM
-	movwf	TBLPTRU, A	; load upper bits to TBLPTRU
-	movlw	high(Table)	; address of data in PM
-	movwf	TBLPTRH, A	; load high byte to TBLPTRH
-	movlw	low(Table)	; address of data in PM
-	movwf	TBLPTRL, A	; load low byte to TBLPTRL
-	movlw	8		; 16 bytes to read
-	movwf 	counter, A	; our counter register
-loop:
-	tblrd*+
-	movf    TABLAT, W, A            ; put byte in WREG for transmit
-	call    SPI_MasterTransmit      ; shifts 8 bits on SDO2 with SCK2
-	
-	decfsz  counter, A
-	bra	loop
-	
-	goto    0
+; Scratch bytes
+COUNT   EQU 0x21
+TEMP    EQU 0x20
+
+psect   code, abs
+main:
+    org 0x0
+    goto start
+
+    org 0x100
+
+; -------------------------
+; SPI2_Init
+; CKP=1 (idle HIGH), CKE=0 (change on falling, stable on rising)
+; Master, Fosc/64
+; -------------------------
+SPI2_Init:
+    bcf   TRISD, 4, A            ; RD4 as output (SDO2)
+    bcf   TRISD, 6, A            ; RD6 as output (SCK2)
+
+    clrf  SSP2STAT, A            ; CKE=0, SMP=0, clear BF
+    ; SSP2CON1: [WCOL SSPOV SSPEN CKP SSPM3 SSPM2 SSPM1 SSPM0]
+    ;                         1     1     0     0     1     0  = 0x32
+    movlw 0x32
+    movwf SSP2CON1, A            ; Enable SPI2, CKP=1, Master Fosc/64
+    return
+
+; -------------------------
+; SPI2_SendByte
+; IN: W = byte to transmit (MSB first)
+; HW auto-generates 8 clocks on RD6 and shifts out on RD4
+; -------------------------
+SPI2_SendByte:
+    movwf SSP2BUF, A             ; start transfer
+Wait_BF:
+    btfss SSP2STAT, 0, A         ; BF bit set when done
+    bra   Wait_BF
+    movf  SSP2BUF, W, A          ; dummy read to clear BF
+    return
+
+; -------------------------
+; Crude delay ~milliseconds (tune constants for your Fosc)
+; IN: W ~= ms
+; -------------------------
+Delay_ms:
+    movwf COUNT, A
+D1: movlw 0xFF
+    movwf TEMP, A
+D2: nop
+    nop
+    decfsz TEMP, F, A
+    bra   D2
+    decfsz COUNT, F, A
+    bra   D1
+    return
+
+; -------------------------
+; Main demo: send 0xAA, 0x55 alternately
+; LEDs will "jump" between two patterns
+; -------------------------
+start:
+    rcall SPI2_Init
+
+Demo:
+    movlw 0x55
+    rcall SPI2_SendByte
+    movlw 0xFF
+    rcall Delay_ms
+    
+    movlw 0xAA
+    rcall SPI2_SendByte
+    movlw 0xFF
+    rcall Delay_ms
+
+    bra Demo
+    end
