4815a1a5b1ae0c5e6aea7290648588b2ae41d470
IC\ms1923 <mahdi97779@gmail.com>
2025-11-21 12:09:45 +0000

    working main

--- main.s
+++ main.s
@@ -1,30 +1,23 @@
 #include <xc.inc>
-extrn   SPI1_Init, DAC_WriteWord
+extrn   SPI1_Init, DAC_WriteWord,SPI1_SendByte
 extrn   DAC_high, DAC_low
 
 psect   code, abs
-rst:    org 0x0000
+        org 0x0000
         goto start
 
         org 0x0100
 start:
         call    SPI1_Init
 
-        ; Example 1: mid-scale (~0.5 * Vref)
-        movlw   0x80
+        ; Example 1: mid-scale (~0.5 * Vref) 
+	movlw   0x40
+	;call	SPI1_SendByte
         movwf   DAC_high, A
-        movlw   0x00
+        movlw   0x40
         movwf   DAC_low, A
-        call    DAC_WriteWord
+loop:	call    DAC_WriteWord
 
-        ; Now change it after a bit if you like
-        ; (add a crude delay loop here)
-        ; movlw 0xFF
-        ; movwf DAC2_hi, A
-        ; movlw 0xFF
-        ; movwf DAC2_lo, A
-        ; call DAC2_WriteWord
+        bra    loop
 
-        goto    $
-
-        end rst
+        end 
